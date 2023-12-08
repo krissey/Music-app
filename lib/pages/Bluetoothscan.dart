3284cc75fc6336af1c8dfa1e20351962376562e9
Krissey <stinemunene@gmail.com>
2023-12-08 11:12:41 +0300

    added bluetooth controller and a bluetooth scan view

---
+++ lib/pages/Bluetoothscan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:musicapp/controllers/Bluetooth_controller.dart';
+
+class Bluetoothscanview extends GetView<BluetoothController>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: ,
+    );
+
+  }
+}
